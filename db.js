43136b390933b2e561c7377de7eb8672d425097d
Casper Sander Hansen <caspersanderhansen@MacBook-Pro-tilhrende-Casper.local>
2026-03-20 15:21:12 +0100

    fix db ssl

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Root9593",
-  database: "taskapp_db",
+  host: "process.env.DB_HOST",
+  user: "process.env.DB_USER",
+  password: "process.env.DB_PASSWORD",
+  database: "process.env.DB_NAME",
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
 });
 
 module.exports = pool.promise();
