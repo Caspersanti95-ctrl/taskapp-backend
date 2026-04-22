8f50d5f3d658d17b8a0ddc7e2c6bdf0d14182284
Casper Hansen <casper.santi95@gmail.com>
2026-04-22 11:04:23 +0200

    stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Stripe = require('stripe');
 
-const stripe = new Stripe0o(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 router.post('/create-checkout-session', async (req, res) => {
     console.log("CREATE CHECKOUT SESSION BODY:", req.body);
