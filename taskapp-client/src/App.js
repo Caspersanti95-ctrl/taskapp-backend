3dfb5fef304b2a193a46e8509aa7133b03a839e5
Casper Hansen <casper.santi95@gmail.com>
2026-04-24 10:18:52 +0200

    fix modal

--- taskapp-client/src/App.js
+++ taskapp-client/src/App.js
@@ -1231,9 +1231,7 @@ function App() {
           <Route 
             path="/tasks/new" 
             element={
-              <RequirePro>
-                <ServiceReportPage />
-              </RequirePro>
+                <ServiceReportPage />              
             } 
           />
 
