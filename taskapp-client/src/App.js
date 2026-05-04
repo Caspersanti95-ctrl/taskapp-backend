3e921be31690b0f04c26f1d6190c19df18f1f02b
Casper Hansen <casper.santi95@gmail.com>
2026-05-04 08:53:11 +0200

    tasks2

--- taskapp-client/src/App.js
+++ taskapp-client/src/App.js
@@ -1269,14 +1269,14 @@ function Dashboard() {
     )}
 
     {createModalOpen && (
-      <CreateTaskModal 
+      <TaskDetailPage 
         onClose={() => setCreateModalOpen(false)}
         fetchTasks={fetchTasks}
       />
      )}
 
      {showProModal && (
-      <ProModal
+      <TaskDetailPage
         onClose={() => setShowProModal(false)}
         onUpgrade={() => navigate("/upgrade")}
       />
