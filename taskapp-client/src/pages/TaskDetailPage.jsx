c8f3294c01501d2aae5a8d70c2a15dda0d6c7d2c
Casper Hansen <casper.santi95@gmail.com>
2026-05-05 12:19:59 +0200

    sda

--- taskapp-client/src/pages/TaskDetailPage.jsx
+++ taskapp-client/src/pages/TaskDetailPage.jsx
@@ -135,7 +135,7 @@ export default function TaskDetailPage() {
             onBlur={saveRemarks}
           />
 
-          {isNew && (
+          {id === "new" && (
             <button 
                 onClick={saveTask}
                 style={{
